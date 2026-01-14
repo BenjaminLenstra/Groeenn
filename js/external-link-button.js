f4367f7e43a5b908cb459aa06487b76c4c8221b3
BenjaminLenstra <lenstra.benjamin@gmail.com>
2026-01-14 22:01:57 +0100

    Change button background color in external link config

--- js/external-link-button.js
+++ js/external-link-button.js
@@ -1,7 +1,7 @@
 // Configuration for the external link button
 var externalLinkConfig = {
     url: 'https://raw.githubusercontent.com/BenjaminLenstra/wegen_draft/96e047ebc74735329735b0fcea6ca36befd3ca70/Ondersteunend%20rapport.pdf',  // Change this to your desired URL
-    backgroundColor: '#36eb1a',      // Button background color
+    backgroundColor: '#195410',      // Button background color
     icon: 'fas fa-external-link-alt'  // FontAwesome icon class
 };
 
@@ -42,5 +42,6 @@ L.Control.ExternalLinkButton = L.Control.extend({
 L.control.externalLinkButton = function(opts) {
     return new L.Control.ExternalLinkButton(opts);
 };
+
 
 
